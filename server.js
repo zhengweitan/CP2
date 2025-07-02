b59e988c4d32dec0921a14bb67bed423b2bd0bc2
zhengweitan <167830757+zhengweitan@users.noreply.github.com>
2025-07-03 00:18:29 +0800

    Fix PORT binding for Render

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(bodyParser.json());
